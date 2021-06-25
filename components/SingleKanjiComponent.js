b298dfab89d5d4dc49770c33f24d2d69b2dfa536
Balu Babu <balub997@gmail.com>
2021-06-26 01:54:17 +0530

    Completed base single kanji component

--- components/SingleKanjiComponent.js
+++ components/SingleKanjiComponent.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Heading, VStack, HStack } from "@chakra-ui/react";
 
 function SingleKanjiComponent() {
   return (
-    <div>
-      <h1>dkshcdsbc</h1>
-    </div>
+    <VStack flex="1" spacing={10} align="center">
+      <Heading pt="20" size="xl">
+        Onyomi
+      </Heading>
+      <Heading fontSize="164" pt="90">
+        動物
+      </Heading>
+      <HStack spacing={40} pt="150">
+        <Heading size="xl">どうぶつ</Heading>
+        <Heading size="xl">Animal</Heading>
+      </HStack>
+    </VStack>
   );
 }
 
