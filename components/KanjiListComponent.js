e223af4214b420c39e91f1d44608ac603f9c03b8
Balu Babu <balub997@gmail.com>
2021-06-26 01:45:47 +0530

    Created the basic layout

---
+++ components/KanjiListComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function KanjiListComponent() {
+  return (
+    <div>
+      <h1>dkshcdsbc</h1>
+    </div>
+  );
+}
+
+export default KanjiListComponent;
---
+++ components/SingleKanjiComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SingleKanjiComponent() {
+  return (
+    <div>
+      <h1>dkshcdsbc</h1>
+    </div>
+  );
+}
+
+export default SingleKanjiComponent;
